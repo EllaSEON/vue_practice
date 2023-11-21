efee91056a1889855d5f13b4996a225f223ddecb
EllaSEON <shineunhye5@gmail.com>
2023-11-21 21:57:39 +0900

    <feat> : click 버튼 달기

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <a v-for="menu in menus" :key="menu">{{ menu }}</a>
+  </div>
+  <div v-for="(product, i) in products" :key="product">
+    <h4 :style="style">{{ product }}</h4>
+    <p>50 만원</p>
+    <button @click="increase(i)">허위매물 신고</button><br />
+    <span>신고수 : {{ 신고수[i] }}</span>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      style: "color:red",
+      menus: ["Home", "Products", "About"],
+      신고수: [0, 0, 0],
+    };
+  },
+  methods: {
+    increase(index) {
+      this.신고수[index] += 1;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +37,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
